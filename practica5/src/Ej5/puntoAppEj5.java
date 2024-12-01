fe9b7fbe482e0b188fb46095e086a5c24f844c65
Ismail Ozkan <ismail98ozkan@gmail.com>
2024-12-01 15:46:38 +0100

    estructura creada en punto y puntoApp

--- practica5/src/Ej5/puntoAppEj5.java
+++ practica5/src/Ej5/puntoAppEj5.java
@@ -1,4 +1,21 @@
 package Ej5;
 
 public class puntoAppEj5 {
+    public static void main(String[] args) {
+        puntoEj5 punto = new puntoEj5(2,1);
+        puntoEj5 puntoTest = new puntoEj5(79, 23);
+
+
+        punto.muestra();
+        punto.desplaza(3,7);
+        punto.muestra();
+        punto.desplazaX(4);
+        punto.muestra();
+        punto.desplazaY(2);
+        punto.muestra();
+
+        System.out.println("distancia entre puntos es de: " + punto.distanciaEuclidea(puntoTest));
+        punto.muestra();
+    }
+
 }
--- practica5/src/Ej5/puntoEj5.java
+++ practica5/src/Ej5/puntoEj5.java
@@ -1,4 +1,37 @@
 package Ej5;
 
 public class puntoEj5 {
+    //variables
+    private double x;
+    private double y;
+
+    //constructo
+    public puntoEj5(double dx, double dy) {
+        this.x = dx;
+        this.y = dy;
+    }
+    //metodos que desplazan en eje concreto
+    public void desplazaX(double dx) {
+        x += dx;
+    }
+    public void desplazaY(double dy) {
+        y += dy;
+    }
+    //metodos que desplazan en 2 ejes al mismo tiempo
+    public void desplaza(double dx, double dy) {
+        x += dx;
+        y += dy;
+    }
+    //mide distancia entre 2 puntos
+    double distanciaEuclidea(puntoEj5 otro) {
+        double dx = this.x - otro.x;
+        double dy = this.y - otro.y;
+        double dist = Math.sqrt(dx * dx + dy * dy);
+        return dist;
+    }
+    //muestra posicion actual del punto
+    public void muestra() {
+        System.out.println("Punto se encuentra en /coord X = " + x + " /coord Y = " + y);
+    }
+
 }
